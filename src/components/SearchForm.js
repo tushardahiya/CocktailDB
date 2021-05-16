836a1e3b47e152d262783a2a05ac48c9d59c6275
tushardahiya <tushardahiya84@gmail.com>
2021-05-16 16:06:32 +0530

    added searching functionality

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -2,12 +2,27 @@ import React from "react";
 import { useGlobalContext } from "../context";
 
 const SearchForm = () => {
-  const { setSeachTerm } = useGlobalContext();
+  const { setSearchTerm } = useGlobalContext();
+  const searchValue = React.useRef("");
+
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value);
+  };
 
   return (
-    <div>
-      <h2>search form component</h2>
-    </div>
+    <section className="section search">
+      <form className="search-form">
+        <div className="form-control">
+          <label htmlFor="label">Search Your Favourite Cocktail</label>
+          <input
+            type="text"
+            id="name"
+            ref={searchValue}
+            onChange={searchCocktail}
+          />
+        </div>
+      </form>
+    </section>
   );
 };
 
