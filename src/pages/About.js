952e1c3d5cecd184ef871b86f579ec30a3c578af
tushardahiya <tushardahiya84@gmail.com>
2021-05-20 14:01:44 +0530

    fixed about.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,7 +4,13 @@ const About = () => {
   return (
     <section className="section about-section">
       <h1 className="section-title">About Us</h1>
-      <p>This is a reactjs project ...blah blah blah</p>
+      <p>
+        This is a webb App made with <span className="about-span">ReactJS</span>{" "}
+        and
+        <span className="about-span"> TheCocktailDB API</span> to help you find
+        your favourite cocktails and learn more about them like their
+        ingredients and instructions
+      </p>
     </section>
   );
 };
